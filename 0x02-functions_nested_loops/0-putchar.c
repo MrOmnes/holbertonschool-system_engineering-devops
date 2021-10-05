81e65731771a5ee79f93c993aa361d87b07494ce
MrOmnes <3644@holbertonschool.com>
2021-10-05 08:48:34 +0000

    .

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{   
+
+    char c[8] = "_putchar";
+    int i = 0;
+
+    while (c[i] != '\0')
+    {
+        _putchar(c[i++]);
+    }
+    _putchar('\n');
+
+    return (0);
+
+}
